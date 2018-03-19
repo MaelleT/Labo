e60083b700a888a17ae01e1ae1d750c04cb15a00
MaelleT <maelle.taurand@ac-nantes.fr>
2018-03-19 16:38:00 +0100

    Api d'ajout

--- Bookmarks/controllers/bookmarkController.js
+++ Bookmarks/controllers/bookmarkController.js
@@ -32,8 +32,34 @@ exports.bookmark_detail = function(req, res, next) {
     var id = req.param('id');
     res.send('NOT IMPLEMENTED: one bookmark detail ');
 }
+
 exports.bookmark_ajout = function(req, res, next) {
-    res.send('NOT IMPLEMENTED: bookmark ajout ');
+
+    let url = req.body.url;
+    let titre = req.body.titre;
+
+    console.log(url);
+    const connectionString = 'postgres://postgres:Password1@192.168.1.50:5432/favori';
+
+    const db = new pg.Client(connectionString);
+    db.connect();
+    const query = {
+
+    name: 'fetch-all-task',
+    text: 'INSERT INTO bookmark(url,titre) VALUES($1,$2) ',
+    values : [url,titre]
+    };
+
+    db.query(query, function(err, result){
+        if (err) {
+            console.log(err.stack);
+            res.send('ERROR');
+
+        } else {
+            res.send('OK');
+
+        }
+    });
 }
 
 exports.bookmark_maj = function(req, res, next) {
