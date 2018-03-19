eb33567ea0bfc900631376cd9004cef5fefcd0d3
MaelleT <maelle.taurand@ac-nantes.fr>
2018-03-19 15:30:41 +0100

    Get all bookmarks

--- Bookmarks/controllers/bookmarkController.js
+++ Bookmarks/controllers/bookmarkController.js
@@ -1,9 +1,35 @@
+const pg = require('pg');
+
 
 exports.bookmark_list = function(req, res, next) {
-    res.send('NOT IMPLEMENTED: bookmarks list');
+
+
+    const connectionString = 'postgres://postgres:Password1@192.168.1.50:5432/favori';
+
+    const db = new pg.Client(connectionString);
+    db.connect();
+    const query = {
+
+    name: 'fetch-all-task',
+    text: 'SELECT * FROM bookmark',
+    };
+
+    db.query(query, function(err, result){
+        if (err) {
+            console.log(err.stack);
+            res.send('ERROR');
+
+        } else {
+            res.json(result.rows);
+
+        }
+});
+
+
 }
 
 exports.bookmark_detail = function(req, res, next) {
+    var id = req.param('id');
     res.send('NOT IMPLEMENTED: one bookmark detail ');
 }
 exports.bookmark_ajout = function(req, res, next) {
